ab21f6cd56843051c333d8981b19ee1b085ad580
Vitoria Pereira Klapowsko <vitoriapereiraklapowsko@gmail.com>
2023-11-24 10:58:36 -0300

    Alguns Ajustes

--- 13 - Eventos de Formulário/js/script.js
+++ 13 - Eventos de Formulário/js/script.js
@@ -1,48 +1,47 @@
 let listaAnimais = [
     {
         idAnimal: 1,
-        nome: "Barão",
+        nome: "Jorginho",
         descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dinisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempornisi id lacinia lobortis. Donec in justo mauris. Mauris luctus",
-        img:"./img/img1.webp"
+        img: "./img/img1.webp"
     },
     {
         idAnimal: 2,
         nome: "Barão",
         descricao:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dinisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempornisi id lacinia lobortis. Donec in justo mauris. Mauris luctus",
-            img:"./img/img2.jpg"
-        },
+        img: "./img/img2.jpg"
+    },
     {
         idAnimal: 3,
-        nome: "Barão",
+        nome: "Lilica",
         descricao:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dinisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempornisi id lacinia lobortis. Donec in justo mauris. Mauris luctus",
-            img:"./img/img3.webp"
-        },
+        img: "./img/img3.webp"
+    },
     {
         idAnimal: 4,
-        nome: "Barão",
+        nome: "Pipoca",
         descricao:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dinisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempornisi id lacinia lobortis. Donec in justo mauris. Mauris luctus",
-            img:"./img/img4.jpg"
-        },
+        img: "./img/img4.jpg"
+    },
 
     {
         idAnimal: 5,
         nome: "Barão",
         descricao:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dinisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempornisi id lacinia lobortis. Donec in justo mauris. Mauris luctus",
-            img:"./img/img5.webp"
+        img: "./img/img5.webp"
     },
 ];
-let container = document.querySelector(".container");
 
 const criarCards = () => {
     let container = document.querySelector(".container");
 
     listaAnimais.forEach((elemento) => {
-        let card = 
-        `
+        let card =
+            `
         <div class="card">
         <img class="card-img" src="${elemento.img}"/>
         <h2 class="card-titulo">${elemento.nome}</h2>
@@ -54,4 +53,28 @@ const criarCards = () => {
         container.innerHTML += card;
     });
 };
-criarCards();
+
+window.addEventListener("load", () => {
+    criarCards();
+    let cards = document.querySelectorAll(".card");
+    cards.forEach((elemento) => {
+        elemento.addEventListener("mouseover", () => {
+            elemento.classList.add("change-scale");
+        });
+
+
+        elemento.addEventListener("mouseout", () => {
+            elemento.classList.remove("change-scale");
+        });
+
+        elemento.lastElementChild.addEventListener("click", (event) => {
+            event.preventDefault();
+            alert(elemento.lastElementChild.getAttribute("idAnimal"));
+            window.location.href = "./adotar.html";
+        });
+    });
+});
+
+
+
+
